b50b9543f888995d1fe3ee107225845a8c9604ef
Ömer Faruk Gönen <ofgonen@gmail.com>
2018-12-01 14:32:47 +0300

    Folders , Tags and Layers are created.
    Also created prefabs for player and ground.
    Created Counter script which is very basic stuff.

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,71 @@
+﻿/*
+Created By OFGONEN
+*/
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour {
+
+    #region Variables
+    public static Counter instance;
+    public Text _text_counter;
+
+    public float _time_round;
+
+    public float _time_star_3;
+    public float _time_star_2;
+    public float _time_star_1;
+    public float _time_star_0;
+
+    public float _counter;
+
+    private bool _can_counter;
+
+    #endregion
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    void Start () 
+	{
+        _counter = _time_round;
+        _text_counter.text = "" + (int)_counter;
+        
+    }
+	
+	void Update () 
+	{
+        if (_can_counter)
+        {
+            _counter -= Time.deltaTime;
+            _text_counter.text = "" + (int)_counter;
+        }
+
+        if (_counter <= 0.05f)
+        {
+            _can_counter = false;
+            //Gamelogic EndGame;
+        }
+	}
+
+    #region Methods
+
+    public void TooglePause(int pause)
+    {
+        if(pause == 1)
+        {
+            _can_counter = true;
+        }
+        else
+        {
+            _can_counter = false;
+        }
+
+    }
+    #endregion
+}
